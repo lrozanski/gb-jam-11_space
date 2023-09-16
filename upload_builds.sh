c02cedc05669b5e50cff316d2c7b0c48932883ce
Lech Różański <l.rozanski88@gmail.com>
2023-09-16 19:27:50 +0200

    Updated the upload script; added export presets

---
+++ upload_builds.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+butler push builds/web "lrozanski/gb-jam-11-space:web"
+butler push builds/linux "lrozanski/gb-jam-11-space:linux"
+butler push builds/macos "lrozanski/gb-jam-11-space:macos"
+butler push builds/windows "lrozanski/gb-jam-11-space:windows"
