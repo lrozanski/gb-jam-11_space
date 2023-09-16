54665adc688dfa3b9e1ddf7f81d3c46dee076a4f
Lech Różański <l.rozanski88@gmail.com>
2023-09-16 21:01:38 +0200

    Added events for the building system

--- ui/Cursor.gd
+++ ui/Cursor.gd
@@ -2,14 +2,12 @@ extends Node2D
 
 @onready var map: Map = $"%TileMap"
 
-
-func _ready():
-	pass # Replace with function body.
-
+signal construct_building
+signal demolish_building
 
 func _process(delta):
+	# Movement
 	var bounds: Rect2 = get_viewport_rect()
-	
 	var direction: Vector2 = Vector2.ZERO
 	
 	if Input.is_action_just_pressed("Up"):
@@ -23,3 +21,13 @@ func _process(delta):
 		
 	if bounds.has_point(global_position + direction * Map.TILE_SIZE):
 		global_position += direction * Map.TILE_SIZE
+	
+	# Building
+	if Input.is_action_just_pressed("Confirm"):
+		var tile = map.local_to_map(map.to_local(global_position))
+		var is_building = map.get_cell_tile_data(1, tile) != null
+		
+		if is_building:
+			demolish_building.emit()
+		else:
+			construct_building.emit()
