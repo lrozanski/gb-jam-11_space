f866af0f503f031cedb0ee118841a4777a94a003
Lech Różański <l.rozanski88@gmail.com>
2023-09-24 15:44:34 +0200

    Added a help screen

---
+++ ui/help_screen.gd
@@ -0,0 +1,53 @@
+extends TextureRect
+class_name HelpScreen
+
+@onready var menu_sound: AudioStreamPlayer = $"Audio/Menu"
+@onready var denied_sound: AudioStreamPlayer = $"Audio/Denied"
+@onready var up_arrow: Control = $"VBoxContainer/Top/UpArrow"
+@onready var down_arrow: Control = $"VBoxContainer/Bottom/DownArrow"
+
+var page: int = 0
+var max_page_index: int = -1
+var pages: Array[Node]
+
+
+func _ready():
+	pages = get_node("VBoxContainer/Content").get_children()
+	max_page_index = pages.size() - 1
+	_update_arrows()
+
+
+func _update_arrows():
+	up_arrow.visible = page > 0
+	down_arrow.visible = page < max_page_index
+
+
+func _previous_page():
+	pages[page].visible = false
+	pages[page - 1].visible = true
+
+	page -= 1
+	menu_sound.play()
+	_update_arrows()
+
+
+func _next_page():
+	pages[page].visible = false
+	pages[page + 1].visible = true
+
+	page += 1
+	menu_sound.play()
+	_update_arrows()
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Up"):
+		if page > 0:
+			_previous_page()
+		else:
+			denied_sound.play()
+	elif Input.is_action_just_pressed("Down"):
+		if page < max_page_index:
+			_next_page()
+		else:
+			denied_sound.play()
